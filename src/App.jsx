8a0c1bd59144d0b9a5b0a7b3442ae49359a7303d
rohitmauryax <rohitmaurya152@gmail.com>
2023-12-26 22:31:22 +0530

    first

---
+++ src/App.jsx
@@ -0,0 +1,51 @@
+import "./App.css";
+
+import React, { useEffect, useState } from "react";
+import NewYearCard from "./NewYearCard";
+
+const calculateCountdown = () => {
+  const now = new Date();
+  const newYear = new Date(now.getFullYear() + 1, 0, 1, 0, 0, 0, 0); // Assuming New Year is at midnight on January 1st
+  const timeDifference = newYear - now;
+  return Math.max(0, Math.floor(timeDifference / 1000));
+};
+function App() {
+  const [showCard, setShowCard] = useState(false);
+  const [countdown, setCountdown] = useState(calculateCountdown());
+
+  useEffect(() => {
+    const timerInterval = setInterval(() => {
+      setCountdown(calculateCountdown());
+    }, 1000);
+
+    return () => clearInterval(timerInterval);
+  }, []);
+
+  const handleTap = () => {
+    setShowCard(true);
+  };
+
+  return (
+    <div className="App">
+      <h1>Ready For New Year!!!!</h1>
+      {countdown > 0 && <p>{formatCountdown(countdown)}</p>}
+      {/* {!showCard && <button onClick={handleTap}>Tap</button>}
+      {showCard && <NewYearCard partnerName="Partner's Name" />} */}
+      {countdown === 0 && !showCard && <button onClick={handleTap}>Tap</button>}
+      {showCard && <NewYearCard partnerName="Charis" />}
+    </div>
+  );
+}
+const formatCountdown = (seconds) => {
+  const hours = Math.floor(seconds / 3600);
+  const minutes = Math.floor((seconds % 3600) / 60);
+  const remainingSeconds = seconds % 60;
+
+  const formatTimeUnit = (unit) => String(unit).padStart(2, "0");
+
+  return `Time until New Year: ${formatTimeUnit(hours)}:${formatTimeUnit(
+    minutes
+  )}:${formatTimeUnit(remainingSeconds)}`;
+};
+
+export default App;
---
+++ src/NewYearCard.jsx
@@ -0,0 +1,22 @@
+// NewYearCard.js
+
+import React from "react";
+import "./NewYearCard.css";
+
+const NewYearCard = ({ partnerName }) => {
+  return (
+    <div className="new-year-card">
+      <h1>Happy New Year, {partnerName}!</h1>
+      <p>
+        As we step into the new year together, I want to express my love and
+        gratitude for having you by my side. May this year bring us joy,
+        laughter, and countless beautiful moments. Cheers to our journey
+        together!
+      </p>
+      <p>With all my love,</p>
+      <p>Your Name</p>
+    </div>
+  );
+};
+
+export default NewYearCard;
